81e5f88b21c4fe15ae73f9c14f648eb735dac0c4
maferBML <maria.fernanda.betancourt@correounivalle.edu.co>
2026-05-19 14:12:29 -0500

    todo funciona pero la complejidad esta compleja

---
+++ Nodo.py
@@ -0,0 +1,9 @@
+class Nodo:
+
+    def __init__(self, fila, columna, valor):
+        self.fila = fila
+        self.columna = columna
+        self.valor = valor
+        self.next = None
+
+
--- SparseMatrix.py
+++ SparseMatrix.py
@@ -1,12 +1,12 @@
 from HashTable import HashTable
 
 
-class SpareseMatrix:
+class SparseMatrix:
 
     def __init__(self, filas, columnas):
         self.filas = filas
         self.columnas = columnas
-        self.hashTable = HashTable(100)
+        self.hashTable = HashTable(100003)  # Usamos un número primo grande para reducir colisiones
 
     # MÉTODOS:
     def get(self, fila, columna):
@@ -67,16 +67,7 @@ class SpareseMatrix:
         return suma
 
     def transpose(self):
-        for bucket in self.hashTable.tabla:
-            actual = bucket.head
-
-            while actual is not None:
-                temp = actual.fila
-                actual.fila = actual.columna
-                actual.columna = temp
-                actual = actual.next
-
-    def top_k(self, k):
+        
         todos = []
 
         for bucket in self.hashTable.tabla:
@@ -86,10 +77,76 @@ class SpareseMatrix:
                 todos.append(actual)
                 actual = actual.next
 
-        self.hashTable = HashTable(100)
+        self.hashTable = HashTable(100003)
 
-        for nodo in todos
+        for nodo in todos:
             nuevaFila = nodo.columna
             nuevaColumna = nodo.fila
 
             self.set(nuevaFila, nuevaColumna, nodo.valor)
+
+    def top_k(self, k):
+            nodos = self.get_all_nodes()
+            nodos_ordenados = self.mergeSort(nodos)
+            return nodos_ordenados[:k]
+        
+
+    def get_all_nodes(self):
+            nodos = []
+            for bucket in self.hashTable.tabla:
+                actual = bucket.head
+
+                while actual is not None:
+                    nodos.append(actual)
+                    actual = actual.next
+            return nodos
+
+    def mergeSort(self, lista):
+            if len(lista) <= 1:
+                return lista
+
+            medio = len(lista) // 2
+
+            izquierda = lista[:medio]
+            derecha = lista[medio:] 
+
+            izquierda = self.mergeSort(izquierda)
+            derecha = self.mergeSort(derecha)
+
+            return self.merge(izquierda, derecha)
+
+    def merge(self, izquierda, derecha):
+            resultado = []
+            i = j = 0
+
+            while i < len(izquierda) and j < len(derecha):
+                if izquierda[i].valor > derecha[j].valor:
+                    resultado.append(izquierda[i])
+                    i += 1
+                else:
+                    resultado.append(derecha[j])
+                    j += 1
+
+            while i < len(izquierda):
+                resultado.append(izquierda[i])
+                i += 1
+
+            while j < len(derecha):
+                resultado.append(derecha[j])
+                j += 1  
+
+            return resultado
+    
+    def density(self):
+        total_nodos = 0
+        for bucket in self.hashTable.tabla:
+            actual = bucket.head
+
+            while actual is not None:
+                total_nodos += 1
+                actual = actual.next
+
+        total_elementos = self.filas * self.columnas
+        if total_elementos == 0:
+            return 0
+        return total_nodos / total_elementos
